fef1abbb76bbead8c3eee20a245cd98912d9aa2e
Altobert <alberto.sanmartin@usach.cl>
2022-05-24 11:54:21 -0400

    se agrega unidad de medida al administrador de tablas de django

--- CRUDERP/admin.py
+++ CRUDERP/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Producto, ProductoReceta, Receta, Stock
+from .models import Producto, ProductoReceta, Receta, Stock, UnidadMedida
 
 # Se va registrando el modelo dentro del administrador.
 
@@ -8,5 +8,7 @@ admin.site.register(Producto)
 admin.site.register(ProductoReceta)
 admin.site.register(Receta)
 admin.site.register(Stock)
+admin.site.register(UnidadMedida)
+
 
 
